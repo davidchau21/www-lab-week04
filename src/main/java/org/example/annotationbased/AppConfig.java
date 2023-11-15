16a635bcbff94aabdda1f1ac8bd02c04cec9b1f8
davidchau21 <davidduongxu1@gmail.com>
2023-11-15 22:10:58 +0700

    update

---
+++ src/main/java/org/example/annotationbased/AppConfig.java
@@ -0,0 +1,9 @@
+package org.example.annotationbased;
+
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@ComponentScans("your.package")
+public class AppConfig {
+}
